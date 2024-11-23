3d1b72b5e60abab62e8096e92a95dbde2995e1ba
Pedro Barros <pedro.barros.509@gmail.com>
2024-11-23 00:27:56 -0300

    PicPay-Pedro Implementando os testes unitários do AuthorizationServiceTest.java

---
+++ src/test/java/com/PicpayTransferSystem/services/AuthorizationServiceTest.java
@@ -0,0 +1,59 @@
+package com.PicpayTransferSystem.services;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.PicpayTransferSystem.externalDTOS.PicPayAuthorizationDTO;
+import com.PicpayTransferSystem.externalDTOS.PicPayAuthorizationDTO.Data;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class AuthorizationServiceTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @InjectMocks
+    private AuthorizationService authorizationService;
+
+    @BeforeEach
+    public void setUp() {
+        // Inicializando os mocks
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Should get transaction authorization successfully")
+    public void testGetAuthorizationTransactionSuccess() throws Exception {
+        var jsonResponse = "{\"status\": \"success\", \"data\": {\"authorization\": true}}";
+
+        var responseEntity = ResponseEntity.ok(jsonResponse);
+        when(restTemplate.getForEntity(anyString(), eq(String.class))).thenReturn(responseEntity);
+
+        var authorization = authorizationService.getAuthorizationTransaction();
+
+        assertTrue(authorization);
+    }
+
+    @Test
+    @DisplayName("Should get transaction authorization failed")
+    public void testGetAuthorizationTransactionFailure() throws Exception {
+        when(restTemplate.getForEntity(anyString(), eq(String.class))).thenThrow(new RuntimeException("Error"));
+
+        var authorization = authorizationService.getAuthorizationTransaction();
+
+        assertFalse(authorization);
+    }
+}
